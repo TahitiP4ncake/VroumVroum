7199df79f6a58314bf2faafc2acee5c67ba533db
TahitiP4ncake <dorian.pancake@gmail.com>
2017-03-09 21:24:23 +0100

    upload whole project
    yes

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    //private Vector3 offset;
+    public GameObject Car;
+    public GameObject Muscle;
+
+    private float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 targetPosition;
+    private bool dezoom;
+    private bool zoom;
+
+    public GameObject generator;
+    public Renderer R_car;
+    public Renderer[] R_carList;
+    public GameObject[] lesVoitures;
+    public List<GameObject> voituresPosition;
+
+   // public float ZoomOut;
+	// Use this for initialization
+	void Start () {
+
+       // voituresPosition = new List<GameObject>();
+
+
+             //offset = transform.position - Car.transform.position; 
+    }
+	void Update()
+    {
+        lesVoitures = GameObject.FindGameObjectsWithTag("Player");
+        /*
+        if(dezoom && transform.position.y<150)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(transform.position.x+1.2f, transform.position.y + 1, transform.position.z), ref velocity, 0.1f);
+            //Debug.Log("peut etre");
+        }
+        if(zoom && transform.position.y >60)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(transform.position.x - 1.2f, transform.position.y - 1, transform.position.z), ref velocity, 0.1f);
+            //Debug.Log("zoom");
+        }
+        */
+        //foreach (GameObject Car in lesVoitures)
+        // Debug.Log(lesVoitures);
+        //voituresPosition = generator.GetComponent<List>();
+
+
+        //Debug.Log(voituresPosition.Count);
+        //  voituresPosition = generator.Voitures;
+        //  Debug.Log(voituresPosition);
+    }
+	// Update is called once per frame
+   
+	void FixedUpdate () {
+        //Vector3 leMilieu = transform.position;
+        Vector3 leMilieu = Vector3.zero;
+        foreach (GameObject Car  in lesVoitures)
+        {
+            
+            leMilieu =leMilieu+ Car.transform.position;
+            
+            /*//Vector3.Distance(new Vector3(transform.position.x,0, transform.position.z),new Vector3(Car.transform.position.x,0, Car.transform.position.z));
+            Debug.Log(Vector3.Distance(new Vector3(leMilieu.x, 0, leMilieu.z), new Vector3(Car.transform.position.x, 0, Car.transform.position.z)));
+            if (Vector3.Distance(new Vector3(leMilieu.x, 0, leMilieu.z), new Vector3(Car.transform.position.x, 0, Car.transform.position.z)) > 60)
+            {
+                dezoom = true;
+            }
+            else
+            {
+                dezoom = false;
+            }
+
+            if (Vector3.Distance(new Vector3(leMilieu.x, 0, leMilieu.z), new Vector3(Car.transform.position.x, 0, Car.transform.position.z)) <50)
+            {
+                zoom = true;
+
+            }
+            else
+            {
+                zoom = false;
+            }
+            */
+        }
+        
+            leMilieu = leMilieu / (GameObject.FindGameObjectsWithTag("Player").Length);
+        int dehors = 0;
+        int dedans = 0;
+        foreach (GameObject Car in lesVoitures)
+        {
+            //Debug.Log(Car);
+            Vector3 screenPos = GetComponent<Camera>().WorldToScreenPoint(Car.transform.position);
+            //float test = Vector3.Distance(screenPos, transform.position);
+            //Debug.Log(Screen.width / 100 * 80);
+            float _hauteur = Vector3.Distance(transform.position, leMilieu);
+            float _ecart = Vector3.Distance(new Vector3(leMilieu.x, 0, leMilieu.z), new Vector3(Car.transform.position.x, 0, Car.transform.position.z));
+            //Debug.Log(Vector3.Distance(new Vector3(leMilieu.x, 0, leMilieu.z), new Vector3(Car.transform.position.x, 0, Car.transform.position.z)));
+            
+            if (screenPos.x<=( GetComponent<Camera>().pixelWidth/ 100*10)|| screenPos.x>= (GetComponent<Camera>().pixelWidth / 100*90)|| screenPos.y<= (GetComponent<Camera>().pixelHeight / 100*10)|| screenPos.y>=(GetComponent<Camera>().pixelHeight / 100*90))
+            {
+                dezoom = true;
+                Debug.Log("dezoom");
+                //zoom = false;
+                dehors += 1;
+            }
+            else
+            {
+                dezoom = false;
+            }
+            
+            if (screenPos.x>= (GetComponent<Camera>().pixelWidth / 100*30)&& screenPos.x<=( GetComponent<Camera>().pixelWidth / 100*70)&& screenPos.y>= (GetComponent<Camera>().pixelHeight / 100*30 )&& screenPos.y<= (GetComponent<Camera>().pixelHeight / 100*70))
+            {
+                dedans += 1;
+                zoom = true;
+                Debug.Log("zoom");
+               // dezoom = false;
+            }
+            else
+            {
+                zoom = false;
+            }
+            
+            /*
+            if(Car.GetComponentInChildren<Renderer>().isVisible)
+            {
+                dezoom = true;
+            }
+            */
+        }
+        //leMilieu.x += 50;
+        
+        
+        
+        leMilieu.x += 50;
+        //leMilieu.x = leMilieu.x / GameObject.FindGameObjectsWithTag("Player").Length;
+        //leMilieu.z = leMilieu.z / GameObject.FindGameObjectsWithTag("Player").Length;
+        targetPosition = leMilieu ;
+        targetPosition.y = transform.position.y;
+        //targetPosition.x = targetPosition.y;
+         //targetPosition.x += targetPosition.y/2;
+        //Debug.Log(targetPosition);
+        //targetPosition.x = targetPosition.x + 30;
+
+        if (dehors == 2 && transform.position.y < 150)
+        {
+            targetPosition = Vector3.SmoothDamp(transform.position, new Vector3(transform.position.x + 20f, transform.position.y + 10, transform.position.z), ref velocity, 0.5f);
+            //Debug.Log("peut etre");
+           // targetPosition.x += 20;
+          //  targetPosition.y += 10;
+        }
+        if (dedans == 2 && transform.position.y > 60)
+        {
+           // targetPosition.x -= 20;
+            //targetPosition.y -= 10;
+            targetPosition = Vector3.SmoothDamp(transform.position, new Vector3(transform.position.x - 20f, transform.position.y - 10, transform.position.z), ref velocity, 0.5f);
+            //Debug.Log("zoom");
+        }
+
+        if (lesVoitures.Length != 0)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
+        //transform.position = Car.transform.position + offset;
+
+        
+    }
+    
+}
---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,484 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Collider C_car;
+    private Rigidbody Car;
+    private AudioSource A_car;
+    private float m_MovementInputValue;         // The current value of the movement input.
+    private float m_TurnInputValue;             // The current value of the turn input.
+    private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
+    private string m_TurnAxisName;              // The name of the input axis for turning.
+
+    
+    public float Speed;
+    public float Boost;
+    public float TurnSpeed;
+    public float JumpH;
+    public float startingPitch;
+    public float Gravity;
+    public float Frein;
+    public float Flip;
+    public float tolerance=100;
+
+    private bool BUMP;
+
+    private GamepadManager manager;
+    public SoundManager A_manager;
+    private float distToGround;
+    private float decalage;
+    private float angle;
+    private Vector3 direction;
+    private bool auSol;
+    private float pitch;
+    public ParticleSystem   Smoke;
+    public ParticleSystem   Turbo;
+    public Collider boost;
+    private bool boostOn;
+    public x360_Gamepad gamepad;
+    private Quaternion bonneOrientation;
+    public int papa;
+    
+    private void Awake()
+    {
+        Car = GetComponent<Rigidbody>();
+        
+
+    }
+    void Start()
+    {
+       A_manager.Source.Add(gameObject.GetComponent<AudioSource>());
+       
+        A_car = GetComponent<AudioSource>();
+        
+        manager = GamepadManager.Instance;
+        gamepad = manager.GetGamepad(papa);
+
+        pitch = startingPitch + Random.Range(-0.1f, 0.1f);
+        Smoke.Pause();
+        distToGround = C_car.bounds.extents.y+1;
+        //Debug.Log(distToGround);
+        Turbo.Pause();
+        bonneOrientation = transform.rotation;
+    }
+    void Update()
+    {
+        //Debug.Log(Car.velocity.magnitude);
+        //Debug.Log(boostOn);
+        //float Lx = gamepad.GetStick_L().X;
+       // Debug.Log(gamepad.GetStick_L().X);
+        IsGrounded();
+        //Debug.Log(Car.velocity);
+     //   decalage = new Vector3(0, Mathf.Atan2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * 180 / Mathf.PI, 0);
+        angle = transform.localEulerAngles.y;
+        angle = (angle > 180) ? angle - 360 : angle;
+        // Debug.Log("voiture" + angle);
+        // Debug.Log("stick" + Mathf.Atan2(Input.GetAxis("Vertical"), -Input.GetAxis("Horizontal")) * 180 / Mathf.PI);
+
+
+
+        if ((gamepad.GetStick_L().Y> 0.5 || gamepad.GetStick_L().Y < -0.5 || gamepad.GetStick_L().X < -0.5 || gamepad.GetStick_L().X > 0.5)&& auSol )
+        {
+            direction= new Vector3(0, Mathf.Atan2(gamepad.GetStick_L().Y, -gamepad.GetStick_L().X) * 180 / Mathf.PI, 0);
+            Turn();
+            if (gamepad.GetButton("B") == false)
+            {
+                Move();
+            }
+        }
+        
+         else
+        {
+            // Debug.Log(Car.velocity);
+
+            
+            if (auSol==false)
+            {
+                Car.velocity = new Vector3(Car.velocity.x, Car.velocity.y - 0.05f, Car.velocity.z);
+                Car.AddForce(Car.velocity*2f);
+
+
+                if (Car.velocity.magnitude<5)
+                {
+
+                    Vector3 _good = new Vector3(0, Car.transform.rotation.y, 0);
+                    Vector3 _bad = new Vector3(Car.transform.rotation.x, Car.transform.rotation.y, Car.transform.rotation.z);
+
+
+
+                    Vector3 x = Vector3.Cross(_bad.normalized, _good.normalized);
+                    float theta = Mathf.Asin(x.magnitude);
+                    Vector3 w = x.normalized * theta / Time.fixedDeltaTime;
+                    Quaternion q = transform.rotation * Car.inertiaTensorRotation;
+                    Vector3 T = q * Vector3.Scale(Car.inertiaTensor, (Quaternion.Inverse(q) * w));
+                    Car.AddTorque(T/100 , ForceMode.VelocityChange);
+                    //  transform.rotation = Quaternion.Slerp(transform.rotation, bonneOrientation, 0.1f * Time.deltaTime);
+                }
+                //StartCoroutine(up());
+            }
+            
+           // float step = TurnSpeed * Time.deltaTime;
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, transform.rotation, step/10);
+            /*
+                        while (Car.velocity.magnitude>0.1)
+                        {
+                            Vector3 movement = Car.velocity * 0.5f ;
+
+                            Car.velocity = movement;
+                            //Car.AddForce(movement);
+                        }
+
+                    */
+
+            //  Vector3 movement = Vector3.Lerp(Car.velocity, Vector3.zero, Frein);
+            //    Car.velocity = movement;
+            // Vector3 movement = Vector3.SmoothDamp(Car.velocity, Vector3.zero, ref currentV, Time.deltaTime);
+            //Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+            Smoke.Stop();
+            
+            Turbo.Stop();
+        }
+        if (gamepad.GetButton("B")&& auSol)
+        {
+            marcheArriere();
+        }
+        if (gamepad.GetButton("X")&& auSol==false )
+        {
+            //Debug.Log("yep");
+            BarrelRoll();
+            //GetUp();
+        }
+       if (gamepad.GetButton("L3"))
+        {
+            //GetUp();
+            Jump();
+        }
+       
+        if (gamepad.GetButtonDown("Y"))
+        {
+            Horn();
+        }
+        
+        m_MovementInputValue = gamepad.GetStick_L().Y;
+        m_TurnInputValue = gamepad.GetStick_L().X;
+
+        // OldTurn();
+        
+      
+             
+
+    }
+    private void Turn()
+    {
+
+        float step = TurnSpeed * Time.deltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f, direction.y, 0f);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, turnRotation, step);
+        // transform.eulerAngles = new Vector3(0, Mathf.Atan2(Input.GetAxis("Vertical"), -Input.GetAxis("Horizontal")) * 180 / Mathf.PI, 0);
+        // if ()
+
+    }
+    private void FixedUpdate()
+    {
+        // Move();
+        /*Debug.Log(Car.rotation.z);
+                  if((Car.rotation.z>0.65f ||Car.rotation.z<-0.65)&& auSol)
+                {
+                    GetUp();
+                }
+                */
+        //Run();
+        if (BUMP)
+        {
+            //Debug.Log("oi");
+            Car.AddForce(new Vector3(0,Car.velocity.magnitude), ForceMode.Impulse);
+            BUMP = false;
+        }
+    }
+    private void marcheArriere()
+    {
+        float _hauteur = Car.velocity.y * Time.deltaTime;
+        Vector3 movement = transform.forward * -Speed * Time.deltaTime;
+        movement = -movement / 1.5f;
+
+        movement.y = _hauteur;
+        if (gamepad.GetButton("A"))
+        {
+            movement = movement * Boost;
+            Turbo.Play();
+
+            Smoke.Stop();
+
+        }
+        else
+        {
+            Smoke.Play();
+            Turbo.Stop();
+
+        }
+        Car.AddForce(movement, ForceMode.Impulse);
+    }
+    private void Move()
+    {
+        float _hauteur = Car.velocity.y*Time.deltaTime;
+        Vector3 movement = transform.forward  * -Speed *Time.deltaTime;
+        /*
+        if (gamepad.GetButton("B"))
+        {
+            movement = -movement/2;
+        }
+        */
+        movement.y = _hauteur;
+        //Debug.Log(movement.y);
+        if (gamepad.GetButton("A"))
+        {
+            movement = movement * Boost;
+            Turbo.Play();
+            
+            Smoke.Stop();
+        }
+        else
+        {
+            Smoke.Play();
+            Turbo.Stop();
+            
+        }
+        
+        if(boostOn==true)
+        {
+            movement = movement * 3.5f;
+        }
+
+        // Car.velocity =  movement;
+        Car.AddForce(movement, ForceMode.Impulse);
+
+
+        /*
+
+        Vector3 movement = transform.forward * m_MovementInputValue * Speed * Time.deltaTime;
+        
+       if (Input.GetButton("Fire1"))
+        {
+            movement = movement * Boost;
+        }
+        // Apply this movement to the rigidbody's position.
+       // Debug.Log("on" + movement);
+        Car.MovePosition(Car.position + movement);
+        */
+    }
+    private void OldTurn()
+    {
+
+        /*   if (Input.GetButton("Horizontal") && m_TurnInputValue >0.5)
+           {
+
+               float turn = m_TurnInputValue * TurnSpeed * Time.deltaTime  ;
+               Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+               Car.MoveRotation( Car.rotation * turnRotation );
+           }
+           */
+           
+        if (Input.GetButton("Horizontal") && m_TurnInputValue > 0.5 && angle > -90  && angle <= 90)
+
+        {
+            
+            transform.Rotate(0, TurnSpeed, 0);
+                }
+
+        if (Input.GetButton("Horizontal") && m_TurnInputValue > 0.5 && ((angle<-90)||(angle>90)))
+
+        {
+
+            transform.Rotate(0, -TurnSpeed, 0);
+        }
+
+
+        if (Input.GetButton("Horizontal") && m_TurnInputValue < -0.5 && angle < 90 && angle >= -90)
+
+        {
+
+            transform.Rotate(0, -TurnSpeed, 0);
+        }
+
+
+        if (Input.GetButton("Horizontal") && m_TurnInputValue < -0.5 && ((angle < -90) || (angle > 90)))
+
+        {
+
+            transform.Rotate(0, TurnSpeed, 0);
+        }
+        
+        if(Input.GetButton("Vertical") && m_MovementInputValue >0.5 && angle>=0 && angle<=180)
+        {
+            transform.Rotate(0, TurnSpeed, 0);
+        }
+
+        if (Input.GetButton("Vertical") && m_MovementInputValue > 0.5 && angle < 0 && angle > -180)
+        {
+            transform.Rotate(0, -TurnSpeed, 0);
+        }
+
+        if (Input.GetButton("Vertical") && m_MovementInputValue < -0.5 && angle <= 0 && angle >= -180)
+        {
+            transform.Rotate(0, TurnSpeed, 0);
+        }
+
+        if (Input.GetButton("Vertical") && m_MovementInputValue < -0.5 && angle > 0 && angle < 180)
+        {
+            transform.Rotate(0, -TurnSpeed, 0);
+        }
+
+        /*if (Input.GetButton("Horizontal") && m_TurnInputValue < -0.5)
+        {
+            Debug.Log(m_TurnInputValue);
+            float turn = m_TurnInputValue * TurnSpeed* Time.deltaTime ;
+            Quaternion turnRotation = Quaternion.Euler(0f, turn , 0f);
+            Car.MoveRotation(turnRotation);
+        }
+        */
+        // Determine the number of degrees to be turned based on the input, speed and time between frames.
+        // float turn = m_TurnInputValue * TurnSpeed * Time.deltaTime;
+        /* Vector3 movement = transform.forward * -Speed * Time.deltaTime;
+         if (Input.GetButton("Horizontal")&& Input.GetButton("Vertical")==false)
+         {
+             Car.MovePosition(Car.position + movement);
+             //Debug.Log("turn" + movement);
+         }*/
+
+        // Make this into a rotation in the y axis.
+        // Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        // Apply this rotation to the rigidbody's rotation.
+        //Car.MoveRotation(Car.rotation * turnRotation);
+
+    }
+    private void Horn()
+    {
+        
+            //Debug.Log("coucou");
+        //A_car.pitch = startingPitch + Random.Range(-0.1f,0.1f);
+        A_car.pitch = pitch;
+            Harmony.Play(A_car);
+            
+    }
+    
+     void OnTriggerEnter(Collider boost)
+    {
+        if (boost.gameObject.tag == "BOOST")
+        {
+            boostOn = true;
+        }
+        if(boost.gameObject.tag == "BUMPER")
+        {
+            BUMP = true;
+        }
+    }
+    void OnTriggerExit(Collider boost)
+    {
+        if (boost.gameObject.tag == "BOOST")
+        {
+            boostOn = false;
+        }
+
+    }
+    private void Run()
+    {
+        Car.velocity = (new Vector3(-m_TurnInputValue * Speed,0, m_MovementInputValue * Speed));
+            }
+    private void IsGrounded()
+    {
+        
+        if(Physics.Raycast(transform.position, -transform.up, distToGround+tolerance))
+        {
+
+
+            auSol = true;
+            return;
+        }
+        else
+        {
+            auSol = false;
+        }
+    }
+    private void BarrelRoll()
+    {
+        //float _turn = Input.GetAxis("Horizontal");
+        Car.AddRelativeTorque(new Vector3(1, 0, 0) * Flip );
+        
+    }
+    private void Jump()
+    {
+        if(auSol)
+        {
+            Car.AddForce(transform.up*1.5f, ForceMode.VelocityChange);
+            
+            StartCoroutine(getDown());
+        }
+        Car.AddRelativeTorque(new Vector3(0, 0, 1), ForceMode.VelocityChange);
+    }
+    IEnumerator getDown()
+    {
+        yield return new WaitForSecondsRealtime(1.2f);
+        Car.AddRelativeForce(new Vector3(0,-1,0),ForceMode.Impulse) ;
+        //Debug.Log("stop");
+    }
+/*
+private void GetUp()
+{
+  // Car.velocity=new Vector3 (0,1,0);
+   // Quaternion _angle = Car.transform.rotation;
+    Vector3 _good = new Vector3(0, Car.transform.rotation.y,0);
+    Vector3 _bad = new Vector3 (Car.transform.rotation.x, Car.transform.rotation.y,Car.transform.rotation.z);
+
+    if((_good.magnitude-_bad.magnitude<0.01 && _good.magnitude - _bad.magnitude>0)|| (_good.magnitude - _bad.magnitude<0 && _good.magnitude - _bad.magnitude>-0.01))
+    {
+        Car.velocity = new Vector3(Car.velocity.x, -2, Car.velocity.z);
+    }
+
+    Vector3 x = Vector3.Cross(_bad.normalized, _good.normalized);
+    float theta = Mathf.Asin(x.magnitude);
+    Vector3 w = x.normalized * theta / Time.fixedDeltaTime;
+    Quaternion q = transform.rotation * Car.inertiaTensorRotation;
+    Vector3 T = q * Vector3.Scale(Car.inertiaTensor, (Quaternion.Inverse(q) * w));
+    Car.AddTorque(T, ForceMode.Impulse);
+
+
+}
+*/
+IEnumerator inactif()
+    {
+        yield return new WaitForSecondsRealtime(3);
+        Debug.Log("1");
+        yield return new WaitForSecondsRealtime(3);
+        Debug.Log("2");
+        yield return new WaitForSecondsRealtime(3);
+        Debug.Log("3");
+        yield return new WaitForSecondsRealtime(3);
+        Debug.Log("deconnecte toi");
+    }
+    /*
+    IEnumerator up()
+    {
+        0, Car.transform.rotation.y,0
+        yield return new WaitForSecondsRealtime(0.1f);
+    }
+    */
+}
+
+
+
+
+// Quaternion turnRotation = Quaternion.Euler(0f, _turn, 0f);
+//Car.transform.Rotate(transform.rotation *turnRotation);
+/* transform.Rotate(new Vector3(0, _turn * TurnSpeed*Time.deltaTime, 0));
+
+ Quaternion rotation = Quaternion.Euler(0, _turn, 0);
+ Vector3 direction = rotation * (new Vector3(0, 0, _go*Speed*Time.deltaTime));
+
+ Car.AddRelativeForce(direction);
+ */
+
+    
